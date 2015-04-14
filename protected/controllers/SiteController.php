16e81ecaf8c7b90ab8624b684bd23033d08b8b9e
Ruben <rbntejeda@hotmail.com>
2015-04-13 23:52:50 -0400

    borrar tabla de usuarios

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -29,8 +29,7 @@ class SiteController extends Controller
 	{
 		// renders the view file 'protected/views/site/index.php'
 		// using the default layout 'protected/views/layouts/main.php'
-		$model=Personas::model()->findAll();
-		$this->render('index',array('model' => $model));
+		$this->render('index');
 	}
 
 	/**
